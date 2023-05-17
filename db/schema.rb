586b01f2deba599b50865f645577912e6a26a478
Yash <yash.solo.000@gmail.com>
2023-05-17 18:50:47 +0600

    Add User and Post controller spec files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_114520) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_102750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_114520) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -52,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_114520) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["author_id"], name: "index_users_on_author_id"
   end
 
