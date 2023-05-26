ceb83fc5b5a822cc47d1236680db23d0ccf270b0
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-05-26 11:30:39 +0100

    Add: Ability

---
+++ app/models/post.rb
@@ -0,0 +1,18 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 250 }
+
+  after_save :update_post_counter
+  after_destroy :update_post_counter
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def most_recent_comments
+    comments.last(5)
+  end
+end
