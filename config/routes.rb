1eb8f7e58ff4800e845d6f7664387889b9a45ae4
Yash <yash.solo.000@gmail.com>
2023-05-26 11:45:00 +0600

    Add feature devise for sing-up, sing-in, store hashed password, ask confirm mail and reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  devise_for :users
+  # get 'users/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "users#index"
+  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create] do 
       resources :comments, only: [:new, :create]
-      resources :likes, only: [:create]
+      resources :likes, only: [:new, :create]
     end
   end
+
 end
